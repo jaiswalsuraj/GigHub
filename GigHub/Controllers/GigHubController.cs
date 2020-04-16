a2cc8e5ddd0b6ca1922237c6806eed1ca14a3ede
SURAJ JAISWAL <SURAJAISWAL@hotmail.com>
2020-04-16 22:38:44 +0530

    GigHubController Added

---
+++ GigHub/Controllers/GigHubController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigHubController : Controller
+    {
+        // GET: GigHub
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
